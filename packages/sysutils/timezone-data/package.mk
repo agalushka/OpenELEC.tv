640067b0af01b1c652084e89205032870d449c4b
Andriy Galushka <agalushka@scalea.tech>
2018-04-20 13:52:36 +0300

    timezone-data: update to 2016e

--- packages/sysutils/timezone-data/package.mk
+++ packages/sysutils/timezone-data/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="timezone-data"
-PKG_VERSION="2016a"
+PKG_VERSION="2016e"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Public Domain"
